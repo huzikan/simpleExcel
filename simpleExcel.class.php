ee671b2333d7f41c5f4c272dcc62754d6f89c41c
huzikan <530154856@qq.com>
2016-08-19 14:30:31 +0800

    大文件数据导出

---
+++ simpleExcel.class.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * simple excel export class
+ * @author huzikan@zbj.com
+ */
+class lib_simpleExcel {
+	//数据转换临时目录
+	const TMP_FILE_DIR = './';
+
+	//最大读取字符串长度(2M)
+	const MAX_LEN = 2097152;
+
+	//导出文件名
+	private $fileName;
+
+	//创建临时存储文件
+	private $tmpFile;
+
+	//xls数据头映射
+	private $headerMap;
+
+	//xls数据头内容
+	private $headerStr;
+
+	//xls输出样式
+	private $styleOption = array(
+		//字体大小
+		'fontSize'  => '12pt',
+		//表头背景色
+		'bgColor'   => '#72c5f5',
+		//文本对齐方式
+		'textAlign' => 'left'
+	);
+
+	//EXCEL导出格式映射
+	private $styleMap = array(
+		//文本
+		'text' 		=>'@',
+		//日期
+		'date'		=>'yyyy-mm-dd', 
+		//数字
+		'number'	=>'#0',
+		//浮点数
+		'decimal'	=>'#0.00', 
+		//百分比
+		'percent'	=>'#0.00%',
+	);
+
+	public function __construct($styleOption = array()) {
+		//设置xls样式配置项
+		if (!empty($styleOption)) {
+			$styleKeyMap = array_keys($this->styleOption);
+			foreach ($styleOption as $key => $value) {
+				if (!empty($value) && in_array($key, $styleKeyMap)) {
+					$this->styleOption[$key] = $value;
+				}
+			}
+		}
+	}
+
+	/**
+	 * 设置导出文件名
+	 * @param string $name 文件名
+	 */
+	public function setFileName($name) {
+		if (!empty($name)) {
+			$this->fileName = $name;
+			$this->tmpFile = self::TMP_FILE_DIR . $name . '.tmp';
+			if (file_exists($this->tmpFile)) {
+				unlink($this->tmpFile);
+			}
+		}
+	}
+
+	/**
+	 * 设置导出行数据头
+	 *
+	 * @param array $columnValue 行数据数组
+	 */
+	public function setXlsHeader($headerMap) {
+		if (empty($headerValue)) {
+			$this->headerMap = $headerMap;
+			$this->headerStr = $this->formatXlsColumn($headerMap, true);
+			//输出数据表头
+			$fp = fopen($this->tmpFile, 'a+');
+			fputs($fp, $this->headerStr);
+			fclose($fp);
+		}
+	}
+
+	/**
+	 * 设置导出行数据
+	 *
+	 * @param array $columnList 行数据数组
+	 * @param bool  $exportFlag 是否单行写入
+	 */
+	public function setXlsColumn($columnList, $exportFlag = true) {
+		if (empty($columnList)) {
+			return false;
+		}
+
+		$fp = fopen($this->tmpFile, 'a+');
+		if ($exportFlag) {
+			//转换导出数据
+			foreach ($columnList as $key => $value) {
+				$columnContent = $this->formatXlsColumn($value);
+				fputs($fp, $columnContent);
+			}
+		//单行写入(不推荐)
+		} else {
+			$columnContent = $this->formatXlsColumn($columnList);
+			fputs($fp, $columnContent);
+		}
+
+		fclose($fp);
+
+		return true;
+	}
+
+	/**
+	 * 导出数据文件
+	 */
+	public function exportXlsData() {
+		header("Content-type:application/vnd.ms-excel");
+		header("Content-Disposition:attachment;filename={$this->fileName}.xls");
+		echo "<table width='100%' border='1'>";
+		//分段获取大文件内容
+		$offset = 0;
+		do {
+			$maxLen = self::MAX_LEN;
+			$content = file_get_contents($this->tmpFile, NULL, NULL, $offset, $maxLen);
+			if (!empty($content)) {
+				echo $content;
+				$offset += $maxLen;
+			}
+		} while (!empty($content));
+
+		unlink($this->tmpFile);
+		echo "</table>";
+	}
+
+	/**
+	 * 格式化导出行数据
+	 *
+	 * @param array $columnValue 	行数据数组
+	 * @param  boolean $isHeader 	是否为表头
+	 * @return string $formatStr 	格式化字符串
+	 */
+	private function formatXlsColumn($columnValue, $isHeader = false) {
+		if (empty($columnValue)) {
+			return '';
+		}
+		$formatStr = "<tr>";
+		foreach ($this->headerMap as $key => $value) {
+			if ($isHeader) {
+				$formatStr .= "<td bgcolor='{$this->styleOption['bgColor']}' style='font-size:{$this->styleOption['fontSize']}'>{$value['name']}</td>";
+			} else {
+				$styleType = in_array($value['type'], array_keys($this->styleMap)) ? $value['type'] : 'text';
+				$style = "style='vnd.ms-excel.numberformat:{$this->styleMap[$styleType]};font-size:{$this->styleOption['fontSize']}'";
+				$formatStr .= "<td align='left' {$style}>{$columnValue[$key]}</td>";
+			}
+		}
+		$formatStr .= "</tr>";
+
+		return $formatStr;
+	}
+
+	public function __destruct() {
+		//删除临时存储文件
+		if (file_exists($this->tmpFile)) {
+			unlink($this->tmpFile);
+		}
+	}
+}
---
+++ testDemo.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once('./simpleExcel.class.php');
+
+$headerMap = array(
+    //文本字段
+    "textField"    => array('name' => '文本字段', 'type' => 'text'),
+    //日期字段
+    "dateField"    => array('name' => '日期字段', 'type' => 'date'),
+    //数字字段
+    "numberField"  => array('name' => '数字字段', 'type' => 'number'),
+    //浮点数字段
+    "decimalField" => array('name' => '浮点数字段', 'type' => 'decimal'),
+    //百分比字段
+    "percentField" => array('name' => '百分比字段', 'type' => 'percent')
+);
+
+$simpleExcel = new lib_simpleExcel();
+$simpleExcel->setFileName("测试列表_" . date('Y-m-d'));
+$simpleExcel->setXlsHeader($headerMap);
+
+//输出10000条测试数据
+for ($i = 0; $i < 50; $i++) {
+    $payBackList = array();
+    for ($j = 0; $j < 200; $j++) {
+        $payBackItem = array(
+            //文本字段
+            "textField"    => '2016081720160817200',
+            //日期字段
+            "dateField"    => date('Y-m-d'), 
+            //数字字段
+            "numberField"  => 10000,
+            //浮点数字段
+            "decimalField" => 10065.25,
+            //百分比字段
+            "percentField" => 0.258
+        );
+        $payBackList[] = $payBackItem;
+    }
+
+    $simpleExcel->setXlsColumn($payBackList);
+}
+
+$simpleExcel->exportXlsData();
+
+?>
